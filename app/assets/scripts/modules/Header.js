05c91dcf273ac12d883479c6a06351e70d369633
Krystian <krystian@konfigura.studio>
2024-11-04 12:36:19 +0100

    A lot of changes.

--- app/assets/scripts/modules/Header.js
+++ app/assets/scripts/modules/Header.js
@@ -1,11 +1,17 @@
 export default class Header {
     constructor() {
+        this.page = window.location.pathname;
+
+        // Check if on the main page; exit if not
+        if (this.page !== "/" && this.page !== "/index.html") return;
+
         this.header = document.querySelector("header");
         this.logo = document.querySelector(".header__logo");
         this.initEvents();
     }
 
     initEvents() {
+        console.log("This page is: " + this.page);
         window.addEventListener("scroll", () => {
             console.log(window.scrollY);
             if (window.scrollY > 50) {
@@ -14,6 +20,4 @@ export default class Header {
             }
         })
     }
-
-
 }
